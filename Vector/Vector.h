11eb2adce538b9bc1d609fca4cfe40edafac0fcf
DionysusBenstein <gromovdenis962@gmail.com>
2018-08-20 21:30:19 +0300

    Add ctor

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -4,12 +4,24 @@ class Vector
 public:
 	Vector()
 	{
+		//
+	}
 
+	//temp constructor
+	Vector(const int value, const int size)
+	{
+		this->size = size;
+		arr = new T[this->size];
+
+		for (int i = 0; i < this->size; i++)
+		{
+			arr[i] = value;
+		}
 	}
 	
 	~Vector()
 	{
-
+		delete[] arr;
 	}
 
 private:
