d5bd072ce11e8998b8d812c2e96edee2da8415ab
DionysusBenstein <gromovdenis962@gmail.com>
2018-08-21 06:56:11 +0300

    Add copy constructor

--- Vector/Vector.h
+++ Vector/Vector.h
@@ -1,3 +1,5 @@
+using namespace std;
+
 template <typename T>
 class Vector
 {
@@ -18,10 +20,31 @@ public:
 			arr[i] = value;
 		}
 	}
-	
+
+	//Copy constructor
+	Vector(const Vector& other)
+	{
+		this->size = other.size;
+		this->arr = new T[this->size];
+
+		for (int i = 0; i < this->size; i++)
+		{
+			this->arr[i] = other.arr[i];
+		}
+	}
+
 	~Vector()
 	{
 		delete[] arr;
+	}
+
+	void print()
+	{
+		for (int i = 0; i < this->size; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << endl;
 	}
 
 private:
