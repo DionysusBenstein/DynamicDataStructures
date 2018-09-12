34b87496c0be06c711896c88ea3e6db82a7c59b7
DionysusBenstein <gromovdenis962@gmail.com>
2018-09-12 18:43:39 +0300

    Add function push()

--- Stack/Stack.h
+++ Stack/Stack.h
@@ -8,6 +8,28 @@ public:
 		size = 0;
 	}
 
+	void push(const T value)
+	{
+		T data = new T[size + 1];
+
+		for (int i = 0; i < size; i++)
+		{
+			data[i] = this->data[i];
+		}
+
+		data[i + 1] = value;
+
+		this->data = data;
+		delete[] data;
+		data = nullptr;
+	}
+
+	//temp
+	void print() const
+	{
+
+	}
+
 private:
 	T* data;
 	size_t size;
