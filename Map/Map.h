060754a2e3a607f7c643345f3f8a947cb190271a
DionysusBenstein <gromovdenis962@gmail.com>
2018-09-21 17:27:45 +0300

    Add header description

--- Map/Map.h
+++ Map/Map.h
@@ -1,3 +1,20 @@
+﻿/*******************************************************************/
+
+/* Original File Name: Map.h                                       */
+
+/* Date: ‎21-09-2018                                                */
+
+/* Developer: Dionysus Benstein                                    */
+
+/* Copyright © 2018 Dionysus Benstein. All rights reserved.        */
+
+/* Description: Реализация ассоциативного массива переменного
+   размера														   */
+
+/*******************************************************************/
+
+#pragma once
+
 template <typename K, typename V>
 class Map
 {
